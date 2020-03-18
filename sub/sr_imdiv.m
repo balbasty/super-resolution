18f709c4395c7ee3e63f4b4293e3c1a1d7a77203
Yael Balbastre <y.balbastre@ucl.ac.uk>
2020-03-18 10:19:39 +0000

    Fix (remove old dependency)

--- sub/sr_imdiv.m
+++ sub/sr_imdiv.m
@@ -22,7 +22,7 @@ else
 end
 dimd = numel(dim);
 dim  = dim(1:end-1);
-vs   = padarray(vs(:)', [0 max(0,3-numel(dim))], 'replicate', 'post');
+vs   = sr_padarray(vs(:)', [0 max(0,3-numel(dim))], 'replicate', 'post');
 
 % -------------------------------------------------------------------------
 % Sum finite differences
@@ -33,13 +33,48 @@ for i=1:numel(size(D))
         Xit = select_slices(X, [dimd dimt], {i t}) ./ (vs(i) * Z);
         switch type(t)
             case '+'
-                D = D + cat(i,      -utils.select_slices(Xit, i, 1), ...
-                               -diff(utils.select_slices(Xit, i, 1:(dim(i)-1)), 1, i), ...
-                                     utils.select_slices(Xit, i, dim(i)-1));
+                D = D + cat(i,      -select_slices(Xit, i, 1), ...
+                               -diff(select_slices(Xit, i, 1:(dim(i)-1)), 1, i), ...
+                                     select_slices(Xit, i, dim(i)-1));
             case '-'
-                D = D + cat(i,      -utils.select_slices(Xit, i, 2), ...
-                               -diff(utils.select_slices(Xit, i, 2:dim(i)), 1, i), ...
-                                     utils.select_slices(Xit, i, dim(i)));
+                D = D + cat(i,      -select_slices(Xit, i, 2), ...
+                               -diff(select_slices(Xit, i, 2:dim(i)), 1, i), ...
+                                     select_slices(Xit, i, dim(i)));
         end
     end
 end
+
+function S = select_slices(X, dim, ind)
+% Select a (multidimensional) slice from a (multidimensional) array.
+%
+% FORMAT slice = utils.select_slice(array, dim, ind)
+% array - {array}  input array
+% dim   - {vector} dimension(s) along which to select a slice
+% ind   - {[cell of] vector[s]} indices to select in each dimension
+% slice - {array}  slice
+%
+% EXAMPLES
+% >> % Create 5-dimensional array
+% >> X = rand(10,10,10,10,10);
+% >>
+% >> % Select the 2nd slice along the 3rd dimension
+% >> % Equivalent to: S = X(:,:,2,:,:)
+% >> S = utils.select_slice(X, 3, 2);
+% >>
+% >> % Select the 1st and 2nd slice along the 1st dimension and the 5th
+% >> % along the 4th dimension
+% >> % Equivalent to: S = X([1 2],:,:,5,:)
+% >> S = utils.select_slice(X, [1 4], {[1 2], 5});
+sizeX    = size(X);
+sub      = struct;
+sub.type = '()';
+sub.subs = repmat({':'}, [1 numel(sizeX)]);
+if iscell(ind)
+    assert(numel(dim) == numel(ind));
+    for d=1:numel(dim)
+        sub.subs{dim(d)} = ind{d};
+    end
+else
+    sub.subs{dim} = ind;
+end
+S = subsref(X,sub);
