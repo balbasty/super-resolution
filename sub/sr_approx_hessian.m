fb53f302ae8bbaf46ae4c6be2e677f06e16ce6e4
Yael Balbastre <y.balbastre@ucl.ac.uk>
2020-04-21 18:51:13 +0100

    Slightly refactor precomputed Hessian

--- sub/sr_approx_hessian.m
+++
@@ -1,6 +0,0 @@
-function H = sr_approx_hessian(ydim, xdim, xmat, ymat, opt, slice)
-% FORMAT H = sr_approx_hessian(ydim, xdim, xmat, ymat, opt, slice)
-
-% -------------------------------------------------------------------------
-% Compute approximate Hessian
-H = sr_proj('AtA', ones(ydim, 'single'), xdim, xmat, ymat, opt, slice);
